3f305a4d77a4b6afab87d7f3dc6eab27e0a30283
Vipulqyrus <121607848+Vipulqyrus@users.noreply.github.com>
2024-04-26 20:03:19 +0530

    removed sensitive data

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDXh2hfPpX5i_2-fbh9wvRRp9V_7oam7H8",
-    authDomain: "crypto-ticker-b9220.firebaseapp.com",
-    projectId: "crypto-ticker-b9220",
-    storageBucket: "crypto-ticker-b9220.appspot.com",
-    messagingSenderId: "1022955479551",
-    appId: "1:1022955479551:web:3896a2ab1d85b7597387ca",
-    measurementId: "G-KXMZSBXMJ8"
+    // add your credentials
 };
 
 export default firebaseConfig;
