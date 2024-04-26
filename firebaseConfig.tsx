cbb3d8a10962c4de2800025124f190e21169d43f
vipul agrawal <48291129+vipulagr890@users.noreply.github.com>
2024-04-26 19:52:47 +0530

    Add files via upload

---
+++ firebaseConfig.tsx
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXh2hfPpX5i_2-fbh9wvRRp9V_7oam7H8",
+    authDomain: "crypto-ticker-b9220.firebaseapp.com",
+    projectId: "crypto-ticker-b9220",
+    storageBucket: "crypto-ticker-b9220.appspot.com",
+    messagingSenderId: "1022955479551",
+    appId: "1:1022955479551:web:3896a2ab1d85b7597387ca",
+    measurementId: "G-KXMZSBXMJ8"
+};
+
+export default firebaseConfig;
